8a19e4a6a1176705dc1915afc891574af574f4b0
Mitxel Moriana <moriana.mitxel@gmail.com>
2014-12-15 09:30:00 +0100

    Added search form basic fields in home view
    He añadido a la home view los fields básicos para el form de búsqueda.
    ¡Todavía no es funcional! Falta implementar controlador, view de
    resultados, etc.

--- milprofes/app/lang/en/home.php
+++ milprofes/app/lang/en/home.php
@@ -12,4 +12,16 @@ return array(
 	'donde' => '¿Dónde estás?',
 	'defaultdonde' => 'Avenida Madrid 123, Bilbao',
 	'aprender' => '¿Qué vas a aprender?',
+	'quebuscas' => '¿Buscas profesor o academia?',
+	'profesor' => 'Profesor',
+	'academia' => 'Academia',
+	'escolar' => 'Escolar',
+	'cfp' => 'CFP',
+	'universitario' => 'Universitario',
+	'artes' => 'Artes',
+	'música' => 'Música',
+	'idiomas' => 'Idiomas',
+	'deportes' => 'Deportes',
+	'encontrar' => 'Encontrar'
+
 );
--- milprofes/app/views/home.blade.php
+++ milprofes/app/views/home.blade.php
@@ -2,22 +2,52 @@
 @section('content')
 <div class="home-splash">
   <div class="container">
-    <div class="row">
-      <div class="col-md-offset-4 col-md-4 jumbo">
+    <!-- div class="row">
+      <div class="col-md-offset-4 col-md-4 jumbo" -->
         
         {{ Form::open(array('url' => 'search')) }}
 
-        {{ Form::label('where-label', @trans('home.donde')) }}
-        {{ Form::text('where', @trans('home.defaultdonde')) }}
-
-        {{ Form::label('where', @trans('home.donde')) }}
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="form-group">
+              {{ Form::label('direccion', @trans('home.donde')) }}
+              {{ Form::text('direccion', '', array('class'=>'form-control input-lg','placeholder'=>@trans('home.defaultdonde'))) }}
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-6">
+            <div class="form-group">
+              {{ Form::label('prof_o_acad', @trans('home.quebuscas')) }}
+              <div class="radio"><label>{{ Form::radio('prof_o_acad', 'profesor', true) }} {{ @trans('home.profesor') }}</label></div>
+              <div class="radio"><label>{{ Form::radio('prof_o_acad', 'academia') }} {{ @trans('home.academia') }}</label></div>
+            </div>
+          </div>
+          <div class="col-xs-6">
+            <div class="form-group">
+              {{ Form::label('categoria', @trans('home.aprender')) }}
+              {{ Form::select('categoria', array(
+                'escolar' => @trans('home.escolar'),
+                'cfp' => @trans('home.cfp'),
+                'universitario' => @trans('home.universitario'),
+                'artes' => @trans('home.artes'),
+                'música' => @trans('home.música'),
+                'idiomas' => @trans('home.idiomas'),
+                'deportes' => @trans('home.deportes'),
+              ), 'escolar', array('class'=>'form-control input-lg')) }}
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          {{ Form::submit(@trans('home.encontrar'), array('class'=>'btn btn-default')) }}
+        </div>
 
         {{ Form::close() }}
 
-      </div>
-    </div>
-  </div>
-</div><!-- /.container -->
+      <!--/div>
+    </div-->
+  </div><!-- /.container -->
+</div><!-- /.home-splash -->
 
 <!-- Anunciarme -->
 <div id="action-anunciarme" class="home-actions">
