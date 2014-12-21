a646b1a56e9b10a4af7e2dbd3c48467ad68cfc62
Mitxel Moriana <moriana.mitxel@gmail.com>
2014-12-21 14:11:13 +0100

    Class name correction

--- milprofes/app/database/migrations/2014_12_18_084929_create_teacher_subject_table.php
+++ milprofes/app/database/migrations/2014_12_18_084929_create_teacher_subject_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateSubjectTeacherTable extends Migration {
+class CreateTeacherSubjectTable extends Migration {
 
 	/**
 	 * Run the migrations.
