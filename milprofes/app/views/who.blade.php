5ba8a82a14a93238e7e6adb82c9570450208dcc2
Mitxel Moriana <moriana.mitxel@gmail.com>
2015-02-25 22:07:08 +0100

    final who view

--- milprofes/app/views/who.blade.php
+++ milprofes/app/views/who.blade.php
@@ -4,30 +4,25 @@
     <div class="container-fluid top-padding-70 bottom-padding-150 background-lamp">
         <div class="container">
 
-            <div class="generic-title"><h3>@lang('faqs.title')</h3></div>
+            <div class="generic-title"><h3>@lang('who.title')</h3></div>
 
-            <div class="generic-subtitle">@lang('faqs.subtitle')</div>
+            <div class="generic-subtitle">@lang('who.subtitle')</div>
 
         </div>
     </div>
     <div class="container-fluid bottom-padding-80 background-gblack overflow-allowed">
         <div class="container generic-box top-padding-50 bottom-padding-50 magic-align">
 
-            <div class="col-xs-offset-0 col-xs-12 col-sm-offset-2 col-sm-8">
-                <div class="row faqs-question">@lang('faqs.question1')</div>
-                <div class="row">@lang('faqs.answer1')</div>
-                <div class="row faqs-question">@lang('faqs.question2')</div>
-                <div class="row">@lang('faqs.answer2')</div>
-                <div class="row faqs-question">@lang('faqs.question3')</div>
-                <div class="row">@lang('faqs.answer3')</div>
-                <div class="row faqs-question">@lang('faqs.question4')</div>
-                <div class="row">@lang('faqs.answer4')</div>
+            <div class="col-xs-offset-0 col-xs-12 col-sm-offset-0 col-sm-12 who-padding">
 
-                <div class="row text-center top-buffer-25">
-                    <hr class="hr-faqs">
-                </div>
-                <div class="row text-center">
-                    <a href="{{ url('contactanos') }}" class="btn btn-primary input-lg faqs-btn">@lang('faqs.doubts')</a>
+                <div class="col-xs-12 text-justify">@lang('who.p1')<br><br></div>
+
+                <div class="col-xs-12 text-justify">@lang('who.p2')<br><br></div>
+
+                <div class="col-xs-12 text-justify">@lang('who.p3')<br><br></div>
+
+                <div class="col-xs-12 text-center top-buffer-35">
+                    <span class="who-logo">@lang('who.logo')</span>
                 </div>
             </div>
         </div>
