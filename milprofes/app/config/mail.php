bdb39ec729c74521b1e4192ec480c78f45484c51
Mitxel Moriana <moriana.mitxel@gmail.com>
2015-03-12 07:35:09 +0100

    mail.php (TEMPORAL)

--- milprofes/app/config/mail.php
+++ milprofes/app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.1and1.es',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'mitxel@network30.com', 'name' => 'Mitxel Moriana'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'mitxel@network30.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => '.Netw0rk#',
 
 	/*
 	|--------------------------------------------------------------------------
