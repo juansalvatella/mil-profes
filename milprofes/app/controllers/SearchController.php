cb67decc1f895d62d8bd126e6c1c1a142eb2bdbe
Mitxel Moriana <moriana.mitxel@gmail.com>
2015-03-25 10:29:15 +0100

    duplicated results bug fix

--- milprofes/app/controllers/SearchController.php
+++ milprofes/app/controllers/SearchController.php
@@ -53,12 +53,12 @@ class SearchController extends BaseController
                         ->leftJoin('users', 'users.id', '=', 'teachers.user_id')
                         ->leftJoin('teachers_average_ratings','teachers_average_ratings.teacher_id','=','teachers.id')
                         ->leftJoin('ratings','ratings.teacher_lesson_id','=','teacher_lessons.id')
+                        ->groupBy('teacher_lessons.id')
                         ->where('subject_id', $subj_id)
                         ->whereRaw("MATCH(teacher_lessons.description) AGAINST(? IN BOOLEAN MODE)", array($keywords))
                         ->orderBy('lesson_avg_rating', 'DESC')
                         ->orderBy('teacher_avg_rating', 'DESC')
-                        ->groupBy('teacher_lessons.id')
-                        ->distinct()
+                        ->orderBy('teacher_lessons.id','ASC')
                         ->get(array('teacher_lessons.*', 'users.email', 'users.phone', 'users.avatar', 'users.username', DB::raw('AVG(ratings.value) as lesson_avg_rating'), 'teachers_average_ratings.teacher_avg_rating'));
                 } else {
                     $results = DB::table('teacher_lessons')
@@ -66,11 +66,11 @@ class SearchController extends BaseController
                         ->leftJoin('users', 'users.id', '=', 'teachers.user_id')
                         ->leftJoin('teachers_average_ratings','teachers_average_ratings.teacher_id','=','teachers.id')
                         ->leftJoin('ratings','ratings.teacher_lesson_id','=','teacher_lessons.id')
+                        ->groupBy('teacher_lessons.id')
                         ->where('subject_id', $subj_id)
                         ->orderBy('lesson_avg_rating', 'DESC')
                         ->orderBy('teacher_avg_rating', 'DESC')
-                        ->groupBy('teacher_lessons.id')
-                        ->distinct()
+                        ->orderBy('teacher_lessons.id','ASC')
                         ->get(array('teacher_lessons.*', 'users.email', 'users.phone', 'users.avatar', 'users.username',DB::raw('AVG(ratings.value) as lesson_avg_rating'),'teachers_average_ratings.teacher_avg_rating'));
                 }
             } else { //search all subjects
@@ -84,6 +84,7 @@ class SearchController extends BaseController
                         ->whereRaw("MATCH(teacher_lessons.description) AGAINST(? IN BOOLEAN MODE)", array($keywords))
                         ->orderBy('lesson_avg_rating', 'DESC')
                         ->orderBy('teacher_avg_rating', 'DESC')
+                        ->orderBy('teacher_lessons.id','ASC')
                         ->get(array('teacher_lessons.*', 'users.email', 'users.phone', 'users.avatar', 'users.username',DB::raw('AVG(ratings.value) as lesson_avg_rating'),'teachers_average_ratings.teacher_avg_rating'));
                 } else {
                     $results = DB::table('teacher_lessons')
@@ -94,6 +95,7 @@ class SearchController extends BaseController
                         ->groupBy('teacher_lessons.id')
                         ->orderBy('lesson_avg_rating', 'DESC')
                         ->orderBy('teacher_avg_rating', 'DESC')
+                        ->orderBy('teacher_lessons.id','ASC')
                         ->get(array('teacher_lessons.*', 'users.email', 'users.phone', 'users.avatar', 'users.username', DB::raw('AVG(ratings.value) as lesson_avg_rating'), 'teachers_average_ratings.teacher_avg_rating'));
                 }
             }
@@ -110,6 +112,7 @@ class SearchController extends BaseController
                         ->whereRaw("MATCH(school_lessons.description) AGAINST(? IN BOOLEAN MODE)", array($keywords))
                         ->orderBy('lesson_avg_rating', 'DESC')
                         ->orderBy('school_avg_rating', 'DESC')
+                        ->orderBy('school_lessons.id','ASC')
                         ->get(array('school_lessons.*', 'schools.name', 'schools.email', 'schools.phone', 'schools.logo',DB::raw('AVG(school_lesson_ratings.value) as lesson_avg_rating'),'schools_average_ratings.school_avg_rating'));
                 } else {
                     $results = DB::table('school_lessons')
@@ -120,6 +123,7 @@ class SearchController extends BaseController
                         ->where('subject_id', $subj_id)
                         ->orderBy('lesson_avg_rating', 'DESC')
                         ->orderBy('school_avg_rating', 'DESC')
+                        ->orderBy('school_lessons.id','ASC')
                         ->get(array('school_lessons.*', 'schools.name', 'schools.email', 'schools.phone', 'schools.logo',DB::raw('AVG(school_lesson_ratings.value) as lesson_avg_rating'),'schools_average_ratings.school_avg_rating'));
                 }
             } else { //search all subjects
@@ -132,6 +136,7 @@ class SearchController extends BaseController
                         ->whereRaw("MATCH(school_lessons.description) AGAINST(? IN BOOLEAN MODE)", array($keywords))
                         ->orderBy('lesson_avg_rating', 'DESC')
                         ->orderBy('school_avg_rating', 'DESC')
+                        ->orderBy('school_lessons.id','ASC')
                         ->get(array('school_lessons.*', 'schools.name', 'schools.email', 'schools.phone', 'schools.logo',DB::raw('AVG(school_lesson_ratings.value) as lesson_avg_rating'),'schools_average_ratings.school_avg_rating'));
                 } else {
                     $results = DB::table('school_lessons')
@@ -141,6 +146,7 @@ class SearchController extends BaseController
                         ->groupBy('school_lessons.id')
                         ->orderBy('lesson_avg_rating', 'DESC')
                         ->orderBy('school_avg_rating', 'DESC')
+                        ->orderBy('school_lessons.id','ASC')
                         ->get(array('school_lessons.*', 'schools.name', 'schools.email', 'schools.phone', 'schools.logo',DB::raw('AVG(school_lesson_ratings.value) as lesson_avg_rating'),'schools_average_ratings.school_avg_rating'));
                 }
             }
