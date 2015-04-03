8b8039c0bbb1ac4aa3ab0a097b81fffe5e1acf90
Mitxel Moriana <moriana.mitxel@gmail.com>
2015-04-03 02:52:03 +0200

    Created file to store and centralize constants

---
+++ milprofes/app/config/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    //ej.: Config::get('constants.array.key');
+
+    //Enlaces a redes sociales
+    'social-links' => [
+        'facebook'      => 'https://www.facebook.com/milprofes',
+        'twitter'       => 'https://twitter.com/milprofes',
+        'linkedin'      => 'https://www.linkedin.com/company/milprofes',
+        'googleplus'    => 'https://plus.google.com/117663094295412414722',
+        'youtube'       => 'https://www.youtube.com/channel/UC71di9d1kvH2LFGZ-PehX2g',
+    ],
+
+];
