dddac2df39f8283079b1e43307e155548fb41ed3
Mitxel Moriana <moriana.mitxel@gmail.com>
2015-07-07 00:16:05 +0200

    sitemap fix, css fix

--- app/views/sitemap.php
+++ app/views/sitemap.php
@@ -3,7 +3,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 <?php foreach ($tags as $tag): ?>
     <url>
         <loc><?= $tag['loc'] ?></loc>
